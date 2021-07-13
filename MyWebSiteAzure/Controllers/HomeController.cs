842b690e15167f903b344a41441b840aecd7c208
loic <loic@prepeers.co>
2021-07-13 13:59:40 +0200

    Ajoutez des fichiers projet.

---
+++ MyWebSiteAzure/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebSiteAzure.Models;
+using System.Diagnostics;
+
+namespace MyWebSiteAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Project()
+        {
+            return View();
+        }
+
+        public IActionResult Game()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
